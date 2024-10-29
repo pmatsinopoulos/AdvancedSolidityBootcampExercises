57d0b5aa97d5d6494ea05854a0811220ba3e1c52
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-10-29 10:09:16 +0200

    Do 1 loop only

--- src/Level_1MatrixAddition.sol
+++ src/Level_1MatrixAddition.sol
@@ -11,27 +11,22 @@ contract Level_1MatrixAddition {
 
             for {
                 let i := 0
-            } lt(i, 3) {
+            } lt(i, 6) {
                 i := add(i, 1)
             } {
-                for {
-                    let j := 0
-                } lt(j, 2) {
-                    j := add(j, 1)
-                } {
-                    let itemxPos := add(
-                        0x04,
-                        add(mul(j, 0x20), mul(i, 64)) // 64 size of row
-                    )
-                    let item1 := calldataload(itemxPos) // x[0][0] (i, j)
-                    let itemyPos := add(itemxPos, 192) // 192 size of X or Y table
-                    let item2 := calldataload(itemyPos) // y[0][0]
-                    // add and store result
-                    let pointer := mload(0x40)
-                    mstore(pointer, add(item1, item2))
-                    // prepare free memory pointer for next
-                    mstore(0x40, add(pointer, 0x20))
-                }
+                let itemxPos := add(0x04, mul(i, 0x20))
+
+                let itemx := calldataload(itemxPos) // x[0][0] (i, j)
+
+                let itemyPos := add(itemxPos, 192) // 192 size of X or Y table
+
+                let itemy := calldataload(itemyPos) // y[0][0]
+
+                // add and store result
+                let pointer := mload(0x40)
+                mstore(pointer, add(itemx, itemy))
+                // prepare free memory pointer for next
+                mstore(0x40, add(pointer, 0x20))
             }
 
             return(result, mul(6, 0x20))
