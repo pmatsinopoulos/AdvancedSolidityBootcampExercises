4f9fdc0c1295dcc642434f982450471738b379b3
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-11-03 10:57:55 +0200

    Initial version: gas avg 29471 and deployment cost 203731 deployment size 729

---
+++ src/Level_2ArraySorting.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.18;
+
+// import "forge-std/console.sol";
+
+contract Level_2ArraySorting {
+    function solution(
+        uint256[10] calldata unsortedArray
+    ) external pure returns (uint256[10] memory sortedArray) {
+        //        9, 5, 1, 4, 7, 6, 10, 15, 3, 2
+        //result: 0, 0, 0, 0, 0, 0, 0,   0, 0, 0
+
+        // Algorithm scans unsortedArray once for each element
+        // and tries to find the correct position in "result"
+        // to put the value in. After having found the correct position
+        // it moves all the other elements one position ahead.
+
+        uint256[10] memory result;
+        result[0] = unsortedArray[0];
+
+        for (uint256 i = 1; i < 10; i++) {
+            // console.log("********************** %d", i);
+            uint256 j = 0;
+            while (j < i && result[j] <= unsortedArray[i]) {
+                j++;
+                // console.log("------ %d", j);
+            }
+            // j points at an element in result which is > the
+            // element at unsortedArray. Hence, I have to shift
+            // all elements in result one position to the right.abi
+            // OR
+            // j points after the end of the result
+            // which means that all elements in result are less than
+            // the unsortedArray[i]. In that case we don't do anything
+            // console.log(
+            //     "j %d position of an element which is greater than %d",
+            //     j,
+            //     unsortedArray[i]
+            // );
+
+            if (j < 10) {
+                for (uint256 k = 9; k >= j + 1; k--) {
+                    // console.log(
+                    //     "setting result k + 1 %d to have the value of k %d",
+                    //     k + 1,
+                    //     k
+                    // );
+                    result[k] = result[k - 1];
+                }
+                result[j] = unsortedArray[i];
+            }
+        }
+
+        // for (uint i = 0; i < 10; i++) {
+        //     console.log(result[i]);
+        // }
+
+        return result;
+    }
+}
---
+++ test/Level_2ArraySorting.t.sol
@@ -0,0 +1,155 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+
+import {Level_2ArraySorting} from "../src/Level_2ArraySorting.sol";
+
+contract Level_2ArraySortingTest is Test {
+    Level_2ArraySorting level2;
+
+    function setUp() public {
+        level2 = new Level_2ArraySorting();
+    }
+
+    function test_case_1() public view {
+        uint256[10] memory unsortedArray = [
+            uint256(9),
+            5,
+            1,
+            4,
+            7,
+            6,
+            10,
+            15,
+            3,
+            2
+        ];
+
+        uint256[10] memory sortedArray = level2.solution(unsortedArray);
+
+        assertEq(sortedArray[0], 1);
+        assertEq(sortedArray[1], 2);
+        assertEq(sortedArray[2], 3);
+        assertEq(sortedArray[3], 4);
+        assertEq(sortedArray[4], 5);
+        assertEq(sortedArray[5], 6);
+        assertEq(sortedArray[6], 7);
+        assertEq(sortedArray[7], 9);
+        assertEq(sortedArray[8], 10);
+        assertEq(sortedArray[9], 15);
+    }
+
+    function test_case_2() public view {
+        uint256[10] memory unsortedArray = [
+            uint256(1),
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            9,
+            10,
+            15
+        ];
+
+        uint256[10] memory sortedArray = level2.solution(unsortedArray);
+
+        assertEq(sortedArray[0], 1);
+        assertEq(sortedArray[1], 2);
+        assertEq(sortedArray[2], 3);
+        assertEq(sortedArray[3], 4);
+        assertEq(sortedArray[4], 5);
+        assertEq(sortedArray[5], 6);
+        assertEq(sortedArray[6], 7);
+        assertEq(sortedArray[7], 9);
+        assertEq(sortedArray[8], 10);
+        assertEq(sortedArray[9], 15);
+    }
+
+    function test_case_3() public view {
+        uint256[10] memory unsortedArray = [
+            uint256(15),
+            10,
+            9,
+            7,
+            6,
+            5,
+            4,
+            3,
+            2,
+            1
+        ];
+
+        uint256[10] memory sortedArray = level2.solution(unsortedArray);
+
+        assertEq(sortedArray[0], 1);
+        assertEq(sortedArray[1], 2);
+        assertEq(sortedArray[2], 3);
+        assertEq(sortedArray[3], 4);
+        assertEq(sortedArray[4], 5);
+        assertEq(sortedArray[5], 6);
+        assertEq(sortedArray[6], 7);
+        assertEq(sortedArray[7], 9);
+        assertEq(sortedArray[8], 10);
+        assertEq(sortedArray[9], 15);
+    }
+
+    function test_case_4() public view {
+        uint256[10] memory unsortedArray = [
+            uint256(15),
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            9,
+            10,
+            1
+        ];
+
+        uint256[10] memory sortedArray = level2.solution(unsortedArray);
+
+        assertEq(sortedArray[0], 1);
+        assertEq(sortedArray[1], 2);
+        assertEq(sortedArray[2], 3);
+        assertEq(sortedArray[3], 4);
+        assertEq(sortedArray[4], 5);
+        assertEq(sortedArray[5], 6);
+        assertEq(sortedArray[6], 7);
+        assertEq(sortedArray[7], 9);
+        assertEq(sortedArray[8], 10);
+        assertEq(sortedArray[9], 15);
+    }
+
+    function test_case_5() public view {
+        uint256[10] memory unsortedArray = [
+            uint256(0),
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        ];
+
+        uint256[10] memory sortedArray = level2.solution(unsortedArray);
+
+        assertEq(sortedArray[0], 0);
+        assertEq(sortedArray[1], 0);
+        assertEq(sortedArray[2], 0);
+        assertEq(sortedArray[3], 0);
+        assertEq(sortedArray[4], 0);
+        assertEq(sortedArray[5], 0);
+        assertEq(sortedArray[6], 0);
+        assertEq(sortedArray[7], 0);
+        assertEq(sortedArray[8], 0);
+        assertEq(sortedArray[9], 0);
+    }
+}
+// TDD .... Test-Driven Development
