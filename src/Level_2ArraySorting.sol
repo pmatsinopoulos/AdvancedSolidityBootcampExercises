4f7e6754b8d41492642295776582746fcd7fbe44
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-11-03 15:43:33 +0200

    0.8.27 and removed console.log

--- src/Level_2ArraySorting.sol
+++ src/Level_2ArraySorting.sol
@@ -1,15 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.18;
-
-// import "forge-std/console.sol";
+pragma solidity 0.8.27;
 
 contract Level_2ArraySorting {
     function solution(
         uint256[10] calldata unsortedArray
     ) external pure returns (uint256[10] memory sortedArray) {
-        //        9, 5, 1, 4, 7, 6, 10, 15, 3, 2
-        //result: 0, 0, 0, 0, 0, 0, 0,   0, 0, 0
-
         // Algorithm scans unsortedArray once for each element
         // and tries to find the correct position in "result"
         // to put the value in. After having found the correct position
@@ -19,11 +14,9 @@ contract Level_2ArraySorting {
         result[0] = unsortedArray[0];
 
         for (uint256 i = 1; i < 10; i++) {
-            // console.log("********************** %d", i);
             uint256 j = 0;
             while (j < i && result[j] <= unsortedArray[i]) {
                 j++;
-                // console.log("------ %d", j);
             }
             // j points at an element in result which is > the
             // element at unsortedArray. Hence, I have to shift
@@ -40,20 +33,12 @@ contract Level_2ArraySorting {
 
             if (j < 10) {
                 for (uint256 k = i; k >= j + 1; k--) {
-                    // console.log(
-                    //     "setting result k + 1 %d to have the value of k %d",
-                    //     k + 1,
-                    //     k
-                    // );
                     result[k] = result[k - 1];
                 }
+
                 result[j] = unsortedArray[i];
             }
         }
-
-        // for (uint i = 0; i < 10; i++) {
-        //     console.log(result[i]);
-        // }
 
         return result;
     }
